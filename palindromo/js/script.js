efb47415d4a72849d58d297d24dad0a701ef862f
saraaffro <saraaffro@gmail.com>
2023-11-27 13:15:53 +0100

    feat: function parola palindoma

--- palindromo/js/script.js
+++ palindromo/js/script.js
@@ -3,3 +3,30 @@ Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+// chiedere all'utente di inserire una parola
+const userWord = prompt("Inserisci una parola");
+console.log(userWord);
+
+let wordDivided = userWord.split('');
+console.log(wordDivided);
+
+let wordReverse = wordDivided.reverse();
+console.log(wordReverse);
+
+let wordReverseResult = wordReverse.join('');
+console.log(wordReverseResult);
+
+parolaPalindomaCheck();
+console.log(parolaPalindomaCheck());
+
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function parolaPalindomaCheck(){
+    if(userWord === wordReverseResult){
+        return "è palindroma";
+    } else{
+        return "non è palindroma";
+    } 
+}
+
